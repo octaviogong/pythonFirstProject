31a5078cd3044328d30bede35c4fed594d30aa99
octaviogong <oky9401@gmail.com>
2021-02-26 14:14:43 -0600

    Update

---
+++ Networking/clienteAsistencia.py
@@ -0,0 +1,22 @@
+import socket
+import pickle
+from estudiante import Estudiante
+
+def main ():
+    s = socket.socket()
+
+    host = "3.16.226.150"
+    port = 9999
+
+    s.connect((host, port))
+
+    estudiante = Estudiante("Octavio Gonzalez", "20111430@tecguanajuato.edu.mx", "Octaviogong")
+    estudiante_seriado = pickle.dumps(estudiante)
+
+    s.send(estudiante_seriado)
+
+    res = s.recv(1024)
+
+    print(f"Respuesta: \n\t{res.decode()}")
+
+    s.close()
---
+++ Networking/estudiante.py
@@ -0,0 +1,19 @@
+class Estudiante:
+    __id = ''
+    __nombre = ''
+    __correo = ''
+    __contra = None
+
+    def _init_(self, nombre, correo, contra):
+        self.__nombre = nombre
+        self.__correo = correo
+        self.__contra = contra
+
+    def getNombre(self):
+        return self.__nombre
+
+    def getCorreo(self):
+        return self.__correo
+
+    def getContra(self):
+        return self.__contra
